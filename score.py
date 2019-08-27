f016655dcdefa579c2f9e01732cd389099d74108
eavanvalkenburg <edvan@microsoft.com>
2019-08-27 06:42:09 +0000

    Commit from NB

--- score.py
+++ score.py
@@ -1,26 +1,37 @@
-
-import pickle
 import json
-import numpy
+import pickle
+import numpy as np
+import pandas as pd
+import azureml.train.automl
+from sklearn.externals import joblib
 from azureml.core.model import Model
+
+from inference_schema.schema_decorators import input_schema, output_schema
+from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
+from inference_schema.parameter_types.pandas_parameter_type import PandasParameterType
+
+
+input_sample = np.array({"data": [[0.02731,0.0,7.07,0.0,0.469,6.421,78.9,4.9671,2.0,242.0,17.8,396.9,9.14]]})
+output_sample = np.array([0])
+
 
 def init():
     global model
-    from sklearn.externals import joblib
-
-    # load the model from file into a global object
-    model_path = Model.get_model_path(model_name="house_prices_regression")
+    # This name is model.id of model that we want to deploy deserialize the model file back
+    # into a sklearn model
+    model_path = Model.get_model_path(model_name = 'house_prices_regression')
     model = joblib.load(model_path)
 
 
-# note you can pass in multiple rows for scoring
+@input_schema('data', NumpyParameterType(input_sample))
+@output_schema(NumpyParameterType(output_sample))
 def run(raw_data):
     try:
         data = json.loads(raw_data)['data']
         data = numpy.array(data)
         result = model.predict(data)
-        # you can return any datatype as long as it is JSON-serializable
         return result.tolist()
     except Exception as e:
-        error = str(e)
-        return error
+        result = str(e)
+        return json.dumps({"error": result})
+    return json.dumps({"result": result.tolist()})
